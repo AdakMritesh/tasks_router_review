9a771a1d75e1a72d9c732ab9f277184a1ab6c0e2
MriteshAdak <mriteshadak@outlook.com>
2026-05-08 20:01:24 +0200

    Adding base schema definitions for task-related operations

---
+++ tasks_router/schema/task_base.py
@@ -0,0 +1,11 @@
+"""
+Schema definitions for task-related operations
+"""
+
+from pydantic import BaseModel, Field, AliasChoices
+from datetime import datetime
+
+class Task(BaseModel):
+    title: str
+    status: str = 'todo'
+    due_date: datetime | None = Field(default=None, validation_alias=AliasChoices('dueDate', 'due_date'))
