18df869caf0d27294c8fd182b751b1424a7a47c7
MriteshAdak <mriteshadak@outlook.com>
2026-05-12 14:00:06 +0200

    Initialize database connection and create tables on startup

--- tasks_router/main.py
+++ tasks_router/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 from tasks_router.routers.task_router import router as task_router
 from tasks_router.routers.user_router import router as user_router
 from tasks_router.routers.system_router import router as system_router
+from tasks_router.database.initiate_db import Database, Base
+from tasks_router.database.config_db import settings
+
+db: Database = Database(settings)
+Base.metadata.create_all(bind=db.get_engine())
 
 router = FastAPI(redirect_slashes=False)
 
