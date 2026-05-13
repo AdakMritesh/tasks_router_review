30e1b665d1c37d892bff57dc92890e4e237d3659
MriteshAdak <mriteshadak@outlook.com>
2026-05-13 08:53:09 +0200

    Refactor database configuration to use SSL connection parameters and update connection URL format

--- tasks_router/database/config_db.py
+++ tasks_router/database/config_db.py
@@ -9,11 +9,18 @@ class Settings(BaseSettings):
     username: str = "user"
     password: str = "password"
     database: str = "tasks_db"
-    ssl_mode: str = "verify-full"
+    sslmode: str = "verify-full"
+    sslrootcert: str = "./global-bundle.pem"
 
     model_config = SettingsConfigDict(env_file=".env")
 
     def get_db_url(self) -> str:
-        return f"postgresql://{self.username}:{self.password}@{self.host}:{self.port}/{self.database}"
+        return f"postgresql+psycopg2://{self.username}:{self.password}@{self.host}:{self.port}/{self.database}"
+    
+    def get_conn_args(self) -> dict[str, str]:
+        return {
+            "sslmode": self.sslmode,
+            "sslrootcert": self.sslrootcert
+        }
     
 settings: Settings = Settings()
--- tasks_router/database/initiate_db.py
+++ tasks_router/database/initiate_db.py
@@ -12,6 +12,7 @@ class Database:
 
     def __init__(self, settings: Settings) -> None:
         self.db_url = settings.get_db_url()
+        self.conn_args = settings.get_conn_args()
         self._engine: Engine | None = None
         self._session_factory: sessionmaker[Session] | None = None
 
@@ -21,17 +22,18 @@ class Database:
         if self._engine is None: # Move all these configuration params to a config file.
             self._engine = create_engine(
                 self.db_url,
-                echo=True,           
-                pool_pre_ping=True,  
-                pool_size=10,        
-                max_overflow=20,
+                echo=True,
+                pool_pre_ping=True,
+                connect_args=self.conn_args,
+                pool_size=10,
+                max_overflow=20
             )
         return self._engine
 
     def get_session_factory(self) -> sessionmaker[Session]:
         """Returns a cached sessionmaker bound to the engine."""
         
-        if self._session_factory is None:
+        if self._session_factory is None: # Move all these configuration params to a config file.
             self._session_factory = sessionmaker(
                 self.get_engine(),
                 autocommit=False,
@@ -39,7 +41,8 @@ class Database:
             )
         return self._session_factory
 
-    def get_db(self) -> Generator[Session, None, None]:
+    # Todo: decide if commit and rollback should be handled here or in the service layer.
+    def get_db(self) -> Generator[Session, None, None]: 
         """
         Generates a new database session for each request. The session is closed after use.
         """
