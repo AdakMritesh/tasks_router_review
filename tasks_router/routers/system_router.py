f5b5e439b49def91877749eb99e8dd49987d0451
MriteshAdak <mriteshadak@outlook.com>
2026-05-17 22:09:04 +0200

    Add status code to health check and root endpoints in system_router

--- tasks_router/routers/system_router.py
+++ tasks_router/routers/system_router.py
@@ -1,15 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 router: APIRouter = APIRouter(tags=["System"])
 
-@router.get("/health")
+@router.get("/health", status_code=status.HTTP_200_OK)
 async def health_check() -> dict[str, str]:
     return {
         "service": "tasks-api",
         "status": "healthy"
         }
 
-@router.get("/")
+@router.get("/", status_code=status.HTTP_200_OK)
 async def root() -> dict[str, str | dict[str, str]]:
     return {
         "message": "Welcome to the Tasks API!",
