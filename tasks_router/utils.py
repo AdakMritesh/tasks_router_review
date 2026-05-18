1ef1db8d3bb9841ce397bd29deff6ba78cb265f5
MriteshAdak <mriteshadak@outlook.com>
2026-05-18 07:44:28 +0200

    Rename utility functions for task and user schema conversions

--- tasks_router/utils.py
+++ tasks_router/utils.py
@@ -1,23 +1,33 @@
 """Utility functions"""
 
 from tasks_router.models.task_model import Task as TaskModel
-from tasks_router.schema.task_schema import TaskResponse
+from tasks_router.schema.task_schema import TaskCreate, TaskResponse
 from tasks_router.models.user_model import User as UserModel
 from tasks_router.schema.user_schema import User as UserDTO
 
-def convert_task_model_to_response(task: TaskModel) -> TaskResponse:
+def convert_task_model_to_response_dto(task: TaskModel) -> TaskResponse:
     """Convert TaskModel instance to TaskResponse schema."""
 
     return TaskResponse.model_validate(task)
 
 
-def convert_task_models_to_responses(tasks: list[TaskModel]) -> list[TaskResponse]:
+def convert_task_models_to_responses_dto(tasks: list[TaskModel]) -> list[TaskResponse]:
     """Convert list of TaskModel instances to list of TaskResponse schemas."""
 
-    return [convert_task_model_to_response(task) for task in tasks]
+    return [convert_task_model_to_response_dto(task) for task in tasks]
 
 
-def convert_user_model_to_user_dto_schema(user: UserModel) -> UserDTO:
+def convert_user_model_to_user_schema_dto(user: UserModel) -> UserDTO:
     """Convert UserModel instance to UserDTO schema."""
 
     return UserDTO.model_validate(user)
+
+def convert_task_schema_dto_to_task_model(task: TaskCreate) -> TaskModel:
+    """Convert TaskCreate schema to TaskModel instance."""
+
+    return TaskModel(
+        user_id=task.user_id,
+        title=task.title,
+        status=task.status,
+        due_date=task.due_date if task.due_date else None
+    )
