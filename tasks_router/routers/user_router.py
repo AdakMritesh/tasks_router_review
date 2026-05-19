ff023631db02dd0fc6f4ff2031643f925648b7ce
MriteshAdak <mriteshadak@outlook.com>
2026-05-19 19:16:28 +0200

    Refactor user router to use dependency injection for user services

--- tasks_router/routers/user_router.py
+++ tasks_router/routers/user_router.py
@@ -3,31 +3,43 @@ User management API endpoints for retrieving and creating users.
 """
 
 from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 
 from tasks_router.services.user_service import UserService
-from tasks_router.repositories.user_repo import UserRepository
+# from tasks_router.repositories.user_repo import UserRepository
 from tasks_router.schema.user_schema import User
-from tasks_router.database.initiate_db import Database
-from tasks_router.database.config_db import settings
+# from tasks_router.database.initiate_db import Database
+# from tasks_router.database.config_db import settings
+from tasks_router.dependencies import get_user_services
 
 router: APIRouter = APIRouter(prefix="/users", tags=["Users"])
 
-_db = Database(settings)
+# _db = Database(settings)
 
-@router.get("/{username}", response_model=User, status_code=status.HTTP_200_OK)
-def get_user(username: str, db: Session = Depends(_db.get_db)) -> User:
+@router.get(
+        "/{username}",
+        response_model=User,
+        status_code=status.HTTP_200_OK)
+def get_user(
+    username: str,
+    user_services: UserService = Depends(get_user_services)
+    ) -> User:
     """Endpoint to retrieve a user by username."""
     
-    user_services: UserService = UserService(UserRepository(db))
     user: User | None = user_services.get_user(username)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
 
-@router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
-def create_user(user: User, db: Session = Depends(_db.get_db)) -> User:
+@router.post(
+        "/",
+        response_model=User,
+        status_code=status.HTTP_201_CREATED
+    )
+def create_user(
+    user: User,
+    user_services: UserService = Depends(get_user_services)
+    ) -> User:
     """Endpoint to create a new user."""
 
-    user_services: UserService = UserService(UserRepository(db))
     return user_services.create(user)
